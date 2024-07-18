611b49012ddca8c7cf525bdd62abf267ac9e3071
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2024-07-18 10:31:32 +0900

    update

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/mochi33i/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/mochi33i/keymap.c
@@ -188,7 +188,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                  KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_MINS  ,
     KC_LCTL  , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                  KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , KC_QUOT  ,
     KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     , KC_LBRC  ,              KC_RBRC, KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , MC_RYCST  ,
-    MO(4)  ,  TG(5)     , KC_LALT  , KC_LGUI,LT(1,KC_LNG2),LT(2,KC_SPC),LT(3,KC_LNG1),          LT(2,KC_ENT),KC_BSPC  ,_______   ,_______   , _______ , KC_LNG1  , KC_PSCR
+    MO(4)  ,  TG(5)     , KC_LALT  , KC_LGUI,LT(1,KC_LNG2),LT(2,KC_SPC),LT(3,KC_LNG1),        LT(2,KC_ENT),KC_BSPC  ,_______   ,_______ , _______ , KC_RCTL , KC_PSCR
   ),
 
   //記号レイヤー
@@ -197,7 +197,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     S(KC_DEL), S(KC_Q)  , S(KC_W)  , S(KC_E)  , S(KC_R)  , S(KC_T)  ,                                  S(KC_Y)  , S(KC_U) ,S(KC_I)   , KC_LBRC  , KC_RBRC  , KC_BSLS,
     KC_GRV   , KC_EXLM  , KC_AT    ,   KC_HASH,   KC_DLR  ,   KC_PERC,                                    KC_CIRC, KC_AMPR   ,  KC_ASTR , KC_LPRN  , KC_RPRN  , KC_TILD,
     _______  ,  _______ , _______  , _______  , KC_PLUS   , KC_MINS , KC_LBRC,                   KC_RBRC, KC_EQL , KC_UNDS   , KC_PLUS  , KC_LCBR  , KC_RCBR  , KC_PIPE,
-    _______  ,S(KC_LCTL),S(KC_LALT),S(KC_LGUI), _______  , _______  , _______  ,            _______  , _______  , _______  ,S(KC_RGUI), _______  , S(KC_RALT), _______
+    _______  ,S(KC_LCTL),S(KC_LALT),S(KC_LGUI), _______  , _______  , _______  ,            _______  , _______  , _______  ,S(KC_RGUI), _______  , S(KC_RALT), KC_RSFT
   ),
 
   //テンキーandマウスレイヤー
