bc8ddaba9c1c71ba6d8939def71da9cf866420c2
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2024-09-04 22:02:16 +0900

    update

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/mochi33i/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/mochi33i/keymap.c
@@ -188,7 +188,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                  KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_MINS  ,
     KC_LCTL  , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                  KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , KC_QUOT  ,
     KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     , KC_LBRC  ,              KC_RBRC, KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , MC_RYCST  ,
-    MO(4)  ,  TG(5)     , KC_LALT  , KC_LGUI,LT(1,KC_LNG2),LT(2,KC_SPC),LT(3,KC_LNG1),        LT(2,KC_ENT),KC_BSPC  ,_______   ,_______ , _______ , KC_RCTL , KC_PSCR
+    KC_LGUI  , TG(5)     , KC_LALT  , KC_LGUI,LT(1,KC_LNG2),LT(2,KC_SPC),LT(3,KC_LNG1),        LT(2,KC_ENT),KC_BSPC  ,_______   ,_______ , _______ , KC_RCTL , KC_PSCR
   ),
 
   //記号レイヤー
